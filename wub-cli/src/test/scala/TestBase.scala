1c4db4ad52907904297702a910b525922fd9f9b6
wildpants <492843674@qq.com>
2023-03-06 09:44:12 +0800

    chore: add PrivateMethodTester for TestBase

--- wub-cli/src/test/scala/TestBase.scala
+++ wub-cli/src/test/scala/TestBase.scala
@@ -1,6 +1,9 @@
 package com.wildpants.wub.cli
 
+
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.PrivateMethodTester
 
-trait TestBase extends AnyFunSuite, Matchers
+
+trait TestBase extends AnyFunSuite, Matchers, PrivateMethodTester
