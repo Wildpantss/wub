556974f83d87533bcad30d0e016359e6d2870aae
wildpants <492843674@qq.com>
2023-03-06 11:16:55 +0800

    chore: add inspectTypeName test

--- wub-cli/src/test/scala/parser/MacrosTest.scala
+++ wub-cli/src/test/scala/parser/MacrosTest.scala
@@ -3,7 +3,6 @@ package parser
 
 import Macros.Entr.*
 
-
 class MacrosTest extends TestBase:
 
   /* ------------ Case class types to inspect ------------ */
@@ -16,6 +15,13 @@ class MacrosTest extends TestBase:
 
   /* ---------------- Test definitions ---------------- */
 
+  test("inspectTypeName[T]") {
+    inspectTypeName[Foo] should be("Foo")
+    inspectTypeName[Fax] should be("Fax")
+    inspectTypeName[Bar] should be("Bar")
+    inspectTypeName[Bax] should be("Bax")
+    inspectTypeName[Baz] should be("Baz")
+  }
 
   test("inspectArity[T]") {
     inspectArity[Foo] should be(1)
@@ -25,6 +31,13 @@ class MacrosTest extends TestBase:
     inspectArity[Baz] should be(0)
   }
 
+  test("inspectDefaults[T]") {
+    inspectDefaults[Foo] should be(List(None))
+    inspectDefaults[Fax] should be(List(None, None))
+    inspectDefaults[Bar] should be(List(None, Some("default string")))
+    inspectDefaults[Bax] should be(List(Some(1), Some("default string")))
+    inspectDefaults[Baz] should be(Nil)
+  }
 
   test("inspectFieldNames[T]") {
     inspectFieldNames[Foo] should be(List("p1"))
@@ -32,15 +45,6 @@ class MacrosTest extends TestBase:
     inspectFieldNames[Bar] should be(List("p1", "p2"))
     inspectFieldNames[Bax] should be(List("p1", "p2"))
     inspectFieldNames[Baz] should be(Nil)
-  }
-
-
-  test("inspectDefaults[T]") {
-    inspectDefaults[Foo] should be(List(None))
-    inspectDefaults[Fax] should be(List(None, None))
-    inspectDefaults[Bar] should be(List(None, Some("default string")))
-    inspectDefaults[Bax] should be(List(Some(1), Some("default string")))
-    inspectDefaults[Baz] should be(Nil)
   }
 
 end MacrosTest
